ef9de3f8664d8161048b13291a6e0c50ca574183
raja327 <nrajaram327@gmail.com>
2024-09-23 09:06:40 +0545

    change in info

--- src/pages/home/Info.jsx
+++ src/pages/home/Info.jsx
@@ -1,7 +1,10 @@
 const Info = () => {
   return (
-    <div>
-
+    <div className="flex flex-col items-center">
+      <h1>Who i am</h1>
+      <div className="flex justify-center h-[400px] w-[400px] text-center text-white items-center bg-black rounded-full">
+        <h1>Hi !,this is Rajaram</h1>
+      </div>
 
 
     </div>
