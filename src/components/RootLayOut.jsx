4414d5606b5951a2e695cf8fff688cd81cac8276
Rabyn-Collab <babynshrestha64@gmail.com>
2024-09-24 08:30:37 +0545

    ch

--- src/components/RootLayOut.jsx
+++ src/components/RootLayOut.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router"
 import Header from "./Header"
+import Footer from "../pages/home/Footer"
 
 const RootLayOut = () => {
   return (
@@ -7,6 +8,8 @@ const RootLayOut = () => {
 
       <Header />
       <Outlet />
+
+      <Footer />
 
 
     </>
