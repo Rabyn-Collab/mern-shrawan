df55d7359e17a76acafe934a12484ea9cffbb379
MK627 <mandipkunwar419@gmail.com>
2024-09-27 08:59:49 +0545

    settimeout

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,49 +1,27 @@
-import React, { useState } from 'react';
-import { faker } from '@faker-js/faker';
+import { useState } from "react";
 
 const HomePage = () => {
-  const [posts, setPosts] = useState([]);
 
 
-  const postAdd = () => {
-    const postData = {
-      id: faker.string.uuid(),
-      username: faker.internet.userName(),
-      image: faker.image.avatar(),
-      email: faker.internet.email(),
-      description: faker.lorem.sentences(),
-    };
-    setPosts((prevPosts) => [...prevPosts, postData]);
-  };
+  const [data, setData] = useState();
 
-  const postDelete = (id) => {
-    setPosts((prevPosts) => prevPosts.filter(post => post.id !== id));
-  };
+  setTimeout(() => {
+    setData({
+      name: 'hello jee'
+    });
+
+  }, 3000)
+
 
   return (
-    <>
-      <div className="">
-        {posts.map((post) => (
-          <div key={post.id} className="p-2 border border-gray-200 rounded mb-4">
-            <img src={post.image} alt={post.username} className="w-16 h-16 rounded-full" />
-            <h3>{post.username}</h3>
-            <p>{post.email}</p>
-            <p>{post.description}</p>
-            <button
-              onClick={() => postDelete(post.id)}
-              className="mt-2 p-2 bg-red-500 text-white rounded"
-            >
-              Delete
-            </button>
-          </div>
-        ))}
-      </div>
+    <div>
 
-      <button onClick={postAdd} className="mt-4 p-2 bg-blue-500 text-white rounded">
-        Add Posts
-      </button>
-    </>
-  );
-};
+      <h1>{data?.name}</h1>
 
-export default HomePage;
+
+
+
+    </div>
+  )
+}
+export default HomePage
