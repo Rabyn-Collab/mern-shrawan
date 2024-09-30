8fc0dd6af62dd29e932b1e18b187aa3f72277b00
Rabyn-Collab <babynshrestha64@gmail.com>
2024-09-30 09:03:52 +0545

    ch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,66 @@
+import axios from "axios"
+import { useEffect } from "react";
 import { useState } from "react";
+
+
+
+
+
+
+
+
+
 
 const HomePage = () => {
 
-
   const [data, setData] = useState();
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState();
 
-  setTimeout(() => {
-    setData({
-      name: 'hello jee'
-    });
 
-  }, 3000)
+
+  //   getData LuFunctionSquare
+  //   setLoad axios
+
+
+  // getData no call
+
+  // kehi samayachi
+  // setData
+  // setaLo
+  //   rerender
+
+  //   rerender
+
+  const getData = async () => {
+    try {
+      setLoad(true);
+      const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+      setData(response.data);
+      setLoad(false);
+    } catch (err) {
+      setLoad(false);
+
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+
+  console.log(data);
+
+
+
+
+
+
 
 
   return (
     <div>
 
-      <h1>{data?.name}</h1>
 
 
 
