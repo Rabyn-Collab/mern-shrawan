d98f837d1084e1825669c903758b6ff48f7da594
MK627 <mandipkunwar419@gmail.com>
2024-10-01 21:21:49 +0545

    ui

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,27 +1,39 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CardComponent from "../../components/CardComponent";
+import { Card } from "@material-tailwind/react";
 
 const HomePage = () => {
+  const [data, setData] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState();
 
+  const getData = async () => {
+    try {
+      setLoad(true);
+      const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+      setData(response.data.categories);
+      setLoad(false);
+    } catch (err) {
+      setLoad(false);
+      setError(err.message);
+    }
+  };
 
-  const [data, setData] = useState();
-
-  setTimeout(() => {
-    setData({
-      name: 'hello jee'
-    });
-
-  }, 3000)
-
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
-    <div>
-
-      <h1>{data?.name}</h1>
-
-
-
+    <div className="grid grid-cols-3 gap-4">
+      {data && data.map((val) => {
+        return (
+          <CardComponent val={val} id={val.idCategory} />
+        );
+      })}
 
     </div>
-  )
-}
-export default HomePage
+  );
+};
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       gridTemplateColumns: {
@@ -16,6 +17,5 @@ module.exports = {
     },
   },
   plugins: [],
-
-}
+});
 
