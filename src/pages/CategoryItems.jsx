7c8ab632b706ea369eedc97488fe10c89fb971b5
Rabyn-Collab <babynshrestha64@gmail.com>
2024-10-02 08:26:29 +0545

    ch

--- src/pages/CategoryItems.jsx
+++ src/pages/CategoryItems.jsx
@@ -1,9 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router"
 
 const CategoryItems = () => {
 
   const { category } = useParams();
-  console.log(category);
+  const [data, setData] = useState();
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState();
+
+  const getData = async () => {
+    try {
+      setLoad(true);
+      const response = await axios.get('https://www.themealdb.com/api/json/v1/1/filter.php', {
+        params: {
+          c: category
+        }
+      });
+      setData(response.data);
+      setLoad(false);
+    } catch (err) {
+      setLoad(false);
+      setError(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
+
   return (
     <div>
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,7 +27,7 @@ const HomePage = () => {
     getData();
   }, []);
 
-  console.log(data);
+
 
 
   return (
