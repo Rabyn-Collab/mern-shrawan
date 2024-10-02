8af4ea5e090e6ae4ed07c3166d5259870c4ad46d
Yubraj upadhya <yubach1718@gmail.com>
2024-10-02 08:30:48 +0545

    ch

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,13 +1,58 @@
-import DisplaySection from "./DisplaySection"
-import Info from "./Info"
-import Tech from "./Tech"
+import axios from "axios"
+import { useEffect } from "react";
+import { useState } from "react";
+import CardComponent from "../../components/CardComponent";
+import { Card } from "@material-tailwind/react";
+
+
+
+
+
+
+
+
+
 
 const HomePage = () => {
+
+  const [data, setData] = useState();
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState();
+
+  const getData = async () => {
+    try {
+      setLoad(true);
+      const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+      setData(response.data);
+      setLoad(false);
+    } catch (err) {
+      setLoad(false);
+      setError(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+
+
+
+
+
+
   return (
-    <div className="space-y-11">
-      <DisplaySection />
-      <Tech />
-      <Info />
+    <div className=" grid grid-cols-3 gap-4">
+      {data && data.categories.map((val) => {
+        return (
+          <CardComponent val={val} id={val.idCategory} />
+        );
+
+      })}
+      <CardComponent />
+
+
+
 
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: ["./src/**/.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       gridTemplateColumns: {
-        'auto': 'repeat(auto-fit, minmax(300px, 1fr))'
-      }
+        auto: "repeat(auto-fit, minmax(300px, 1fr))",
+      },
     },
     screens: {
-      '2xl': { 'max': '1536px' },
-      'xl': { 'max': '1280px' },
-      'lg': { 'max': '1024px' },
-      'md': { 'max': '768px' },
-      'sm': { 'max': '640px' },
+      "2xl": { max: "1536px" },
+      xl: { max: "1280px" },
+      lg: { max: "1024px" },
+      md: { max: "768px" },
+      sm: { max: "640px" },
     },
   },
   plugins: [],
-
-}
-
+});
