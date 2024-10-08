054e9c4c07e8a1e343bb718ba4cb5524578ca027
Rabyn-Collab <babynshrestha64@gmail.com>
2024-10-08 08:18:12 +0545

    changes

--- src/pages/AddForm.jsx
+++ src/pages/AddForm.jsx
@@ -1,22 +1,7 @@
-import { Button, Input, Typography } from "@material-tailwind/react"
-import { Formik, useFormik } from "formik";
+import { Button, Checkbox, Input, Option, Radio, Select, Textarea, Typography } from "@material-tailwind/react"
+import { Formik } from "formik";
 
 const AddForm = () => {
-
-  // const { handleChange, handleSubmit, values, touched, errors } = useFormik({
-  //   initialValues: {
-  //     title: ''
-  //   },
-  //   onSubmit: (val) => {
-  //     console.log(val);
-  //   }
-  // });
-
-
-
-
-
-  console.log('render');
 
   return (
     <div className="max-w-[370px] p-5" >
@@ -25,17 +10,19 @@ const AddForm = () => {
       <Typography variant="h5" className="mb-4">Add Post</Typography>
 
       <Formik
-
         initialValues={{
-          title: ''
+          title: '',
+          detail: '',
+          pLang: '',
+          colors: [],
+          country: '',
         }}
         onSubmit={(val) => {
-
           console.log(val);
         }}
       >
 
-        {({ handleChange, handleSubmit, values, touched, errors }) => {
+        {({ handleChange, handleSubmit, values, touched, errors, setFieldValue }) => {
 
           return <form onSubmit={handleSubmit} className="space-y-5">
 
@@ -44,6 +31,56 @@ const AddForm = () => {
                 onChange={handleChange}
                 name="title"
                 label="Title"
+                value={values.title}
+              />
+            </div>
+
+            <div className="flex gap-10">
+              <Radio
+                onChange={handleChange}
+                value={"HTML"}
+                name="pLang" label="HTML" />
+              <Radio
+                onChange={handleChange}
+                value={"REACT"}
+                name="pLang" label="React" />
+            </div>
+
+            <div className="flex w-max gap-4">
+              <Checkbox
+                name="colors"
+                onChange={handleChange}
+                value={"Blue"}
+                color="blue" label="Blue" />
+              <Checkbox
+                name="colors"
+                onChange={handleChange}
+                value={"Red"}
+                color="red" label="Red" />
+
+            </div>
+
+            <div className="w-full">
+              <Select
+                onChange={(e) => setFieldValue("country", e)}
+                name="country"
+                label="Select Country">
+                <Option value="Nepal">Nepal</Option>
+                <Option value="China">China</Option>
+
+              </Select>
+            </div>
+
+
+
+
+
+            <div>
+              <Textarea
+                onChange={handleChange}
+                name="detail"
+                label="Detail"
+                value={values.detail}
               />
             </div>
 
