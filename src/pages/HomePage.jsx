4cb0df52b5d77f55e029bed7ce3a001bbc7cc84e
raja327 <nrajaram327@gmail.com>
2024-10-24 09:23:25 +0545

    change

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,15 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import CardAni from "../components/CardAni";
 //
 const HomePage = () => {
-
   const { posts } = useSelector((state) => state.postSlice);
-
-
-
-
 
   return (
     <div className="p-3 grid grid-cols-3">
-
       {posts.map((post, i) => {
-
         return <CardAni key={post.id} post={post} index={i} />;
       })}
     </div>
-  )
-}
-export default HomePage
+  );
+};
+export default HomePage;
