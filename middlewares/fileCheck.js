378a1f428a1d6238f3182a4406037ed95913b567
Rabyn-Collab <babynshrestha64@gmail.com>
2024-11-12 09:34:41 +0545

    ch

--- middlewares/fileCheck.js
+++ middlewares/fileCheck.js
@@ -1,16 +1,18 @@
 
 
 
-
-
-
 export const someWare = (req, res, next) => {
 
   console.log(req.body);
 
+
+  const m = () => {
+
+  }
   if (req.body.email) {
     return next();
   }
+
 
   return res.status(400).json({ message: 'file not found' });
 
