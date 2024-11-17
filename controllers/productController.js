33308b3ed3bdebb0cc3fd3febcb793bd1dfcfd0d
Rabyn-Collab <babynshrestha64@gmail.com>
2024-11-17 10:03:44 +0545

    create css

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,10 +3,33 @@ import Product from "../models/Product.js"
 
 
 
-export const getAllProducts = async (req, res) => {
+export const getProducts = async (req, res) => {
+
+
+
   try {
-    const response = await Product.find();
-    return res.status(200).json(response);
+
+    // search , sort, fields, operator, limit, page , skip
+
+    const excludesFields = ['sort', 'search', 'limit', 'fields', 'skip', 'page'];
+
+    const queryObj = { ...req.query };
+
+
+    excludesFields.forEach((label) => delete queryObj[label]);
+
+
+    let qStr = JSON.stringify(queryObj);
+
+    qStr = qStr.replace(/\b(gte|gt|lte|lt|eq)\b/g, match => `$${match}`);
+
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const skip = (page - 1) * limit;
+
+    const response = await Product.find(JSON.parse(qStr)).skip(skip).limit(limit);
+
+    return res.status(200).json({ length: response.length, products: response });
   } catch (err) {
     return res.status(400).json({ err: `${err}` });
   }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import { getAllProducts } from '../controllers/productController.js';
+import { getProducts } from '../controllers/productController.js';
 
 const router = express.Router();
 
-router.route('/products').get(getAllProducts);
+router.route('/').get(getProducts);
 // router.route('/products/:id').get().patch().delete();
 
 export default router;
