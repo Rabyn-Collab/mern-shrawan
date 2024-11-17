efc52926b785bba8a46210ac518f42a5795b90e7
Rabyn-Collab <babynshrestha64@gmail.com>
2024-11-17 09:13:27 +0545

    create css

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,13 @@
+import Product from "../models/Product.js"
 
 
-const data = [
-  {
-    "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "qui sunt rem eveniet architecto"
-  },
-  {
-    "id": 2,
-    "title": "qui est esse",
-    "body": "<h1> hhello </h1>"
+
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const response = await Product.find();
+    return res.status(200).json(response);
+  } catch (err) {
+    return res.status(400).json({ err: `${err}` });
   }
-]
-
-export const getAllProducts = (req, res) => {
-  return res.status(200).json(data);
-}
-export const createProduct = (req, res) => {
-  return res.status(200).json({ message: 'creating product' });
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
-import { createProduct, getAllProducts } from '../controllers/productController.js';
+import { getAllProducts } from '../controllers/productController.js';
 
 const router = express.Router();
 
-router.route('/products').get(getAllProducts).post(createProduct);
-router.route('/products/:id').get().patch().delete();
+router.route('/products').get(getAllProducts);
+// router.route('/products/:id').get().patch().delete();
 
 export default router;
 
