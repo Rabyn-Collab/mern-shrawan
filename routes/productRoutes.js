7d312f0395098271e56c123be246bcfaa0694882
Rabyn-Collab <babynshrestha64@gmail.com>
2024-11-21 09:33:48 +0545

    create css

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,15 @@
 import express from 'express';
 
-import { createProduct, getProducts, getTopProducts, removeProduct } from '../controllers/productController.js';
+import { createProduct, getProducts, getTopProducts, removeProduct, updateProduct } from '../controllers/productController.js';
 import { adminCheck, userCheck } from '../middlewares/authCheck.js';
-import { fileCheck } from '../middlewares/fileCheck.js';
+import { fileCheck, updateFileCheck } from '../middlewares/fileCheck.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(userCheck, adminCheck,
   fileCheck, createProduct);
 router.route('/top-5-products').get(getTopProducts, getProducts);
-router.route('/:id').get().patch().delete(removeProduct);
+router.route('/:id').get().patch(userCheck, adminCheck, updateFileCheck, updateProduct).delete(removeProduct);
 
 export default router;
 
