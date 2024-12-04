4da3b0788624475bbdd6b60861d796b37cc748ea
MK627 <mandipkunwar419@gmail.com>
2024-12-04 20:46:03 +0545

    djd

--- index.js
+++ index.js
@@ -9,19 +9,10 @@ const port = 5000;
 import cors from 'cors';
 
 const app = express();
-<<<<<<< HEAD
+
 app.use(cors());
 
-<<<<<<< HEAD
 mongoose.connect('mongodb+srv://kunwarmandip3:admin@cluster0.ngq19.mongodb.net/Shops').then((val) => {
-=======
-=======
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
-app.use(cookieParser());
->>>>>>> 6e1b9dc2ced48ff48013fd8e2e763c3b60911d42
   app.use(express.json());
   app.use(express.static('uploads'));
 
@@ -31,14 +22,11 @@ app.use(cookieParser());
   }));
 
   mongoose.connect('mongodb+srv://rabyn900:moles900@cluster0.ikwdezp.mongodb.net/Shops').then((val) => {
-<<<<<<< HEAD
->>>>>>> 5b5d0e4fa3f6134f14c1e125c8f3c344659922be
+
     app.listen(port, () => {
       console.log('listening and connected');
     });
-=======
 
->>>>>>> 6e1b9dc2ced48ff48013fd8e2e763c3b60911d42
   }).catch((err) => {
     console.log(err);
   });
@@ -58,4 +46,3 @@ app.use(cookieParser());
   app.listen(port, () => {
     console.log('listening and connected');
   });
-
